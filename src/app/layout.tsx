098ef97fe96be3e78c0fd4456181f0be5eaa5023
Kevin <vnctkevin@gmail.com>
2024-02-14 16:56:01 +0700

    bugfixing layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   description: "A portfolio of Kevin's work and experiences.",
 };
 
-export default function Layout({ children }) {
+export default function Layout(children: any) {
   return (
     <div className="bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100">
       {children}
