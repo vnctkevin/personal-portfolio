095b11260a6532946b3a446c4b036e2cf0b2a112
Kevin <vnctkevin@gmail.com>
2024-02-15 01:25:31 +0700

    add google analytics tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,6 +31,16 @@ export default function Index() {
         <meta name="description" content="Kevin's (@vnctkevin) personal portfolio website" />
         <meta name="keywords" content="portfolio, Kevin, software, development, design, vnctkevin" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-G6JD3QFVBB"></script>
+        <script>
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-G6JD3QFVBB');
+        `}
+        </script>
     </Head> 
       <IndexPage />
     </>
