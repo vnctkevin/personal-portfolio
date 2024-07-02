bfd029946a8bfaae65aa7f52df47e4d66d2c2194
Kevin <vnctkevin@gmail.com>
2024-07-02 22:09:29 +0700

    update page

--- components/IndexPage.tsx
+++ components/IndexPage.tsx
@@ -3,10 +3,8 @@
  * @see https://v0.dev/t/qIfaZ6Ne5Gb
  * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
  */
-import Link from "next/link"
 import HeroSection from "./Hero"
 import Navbar from "./Navbar"
-import Image from "next/image"
 import Experiences from "./Experiences"
 import Projects from "./Projects"
 import Footer from "./Footer"
